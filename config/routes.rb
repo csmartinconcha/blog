895fd6790c2f70791e32d44abf0f6a5e6bf2c890
csmartinconcha <claudio_san_martin@hotmail.com>
2021-10-06 00:22:45 -0300

    Usando metodo Reosrces para generar rutas Rest del recurso Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+
+  #get "/articles", to: "articles#index"   .....   antes de utilizar Resorces
+  #get "/articles/:id", to: "articles#show" .......antes de utilizar Resorces   
+
+
+
+  resources :articles
+  
 end
