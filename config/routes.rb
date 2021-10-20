1e766914504bede64969bbba0d1b2121f52d51fc
csmartinconcha <claudio_san_martin@hotmail.com>
2021-10-20 02:02:23 -0300

    Agregando rutas para comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
-
-  #get "/articles", to: "articles#index"   .....   antes de utilizar Resorces
-  #get "/articles/:id", to: "articles#show" .......antes de utilizar Resorces   
-
-
-
-  resources :articles
-  
+  resources :articles do
+   resources :comments
+  end
+ 
 end
+  #get "/articles", to: "articles#index"   .....   antes de utilizar Resorces
+  #get "/articles/:id", to: "articles#show" .......antes de utilizar Resorces
