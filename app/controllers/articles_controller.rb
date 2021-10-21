f41e062c9e86067eaf2f79f48ee37a948f8c9981
csmartinconcha <claudio_san_martin@hotmail.com>
2021-10-21 01:18:37 -0300

    Usando before action para DRY (Dont read yourself)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
 
+before_action : set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    
   end
 
  def new
@@ -22,11 +24,10 @@ class ArticlesController < ApplicationController
 end
 
 def edit  
-  @article = Article.find(params[:id])
+  
 end
 
 def update
-  @article = Article.find(params[:id])
   if @article.update(article_params)
     redirect_to @article
   else
@@ -36,19 +37,23 @@ end
   
 
 def destroy
-  @article = Article.find(params[:id])
-  @article.destroy
+   @article.destroy
   redirect_to articles_path
 end
 
   private
+  @article = Article.find(params[:id])
+  def set_article
+    
+
+
+  end
 
   def article_params
     params.require(:article).permit(:title, :body)
   end 
 
   
-
  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
   resources :articles do
-   resources :comments
+   resources :comments, only: [:create, :destroy]
   end
- 
-end
+ end
   #get "/articles", to: "articles#index"   .....   antes de utilizar Resorces
   #get "/articles/:id", to: "articles#show" .......antes de utilizar Resorces
